ed2753349defb083053ef6160a67695a2b4c26c7
Alexandre Caldato <caldato.alexandre@gmail.com>
2023-09-23 12:22:14 +0200

    added img and fixed layout

--- src/presentation.ts
+++ src/presentation.ts
@@ -1,6 +1,8 @@
+import  img  from "./img.jpg";
+
 export const presentationHtml = `
 <section id="presentation" class="presentation p-5">
-
+    <div class="left-section">
     <div class="section-title">
         <h2 data-aos="fade-down" data-aos-duration="1000"><i class="bi bi-person-circle"></i> Présentation</h2>
         <p data-aos="fade-up" data-aos-duration="1200" data-aos-delay="300">En quelques mots</p>
@@ -20,11 +22,11 @@ export const presentationHtml = `
     </p>
 
     <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1100">
-        <i class="bi bi-patch-check-fill"></i> Outre un soutien psychologique traditionnel, j’offre, lorsque la situation s’y prête, des sessions de thérapies brèves de libération émotionnelle (EFT). <a href="#"><i class="bi bi-info-circle-fill"></i> [En savoir plus]</a>
+        <i class="bi bi-patch-check-fill"></i> Outre un soutien psychologique traditionnel, j’offre, lorsque la situation s’y prête, des sessions de thérapies brèves de libération émotionnelle (EFT).
     </p>
 
     <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1300">
-        <i class="boxicons bx bxs-leaf"></i> Animée par une passion pour la nature et ses bienfaits, je me suis également formée en tant que conseillère en produits de soins naturels. Cette compétence complémentaire me permet de vous suggérer des solutions naturelles pour des problématiques courantes telles que l'anxiété, le stress ou les troubles du sommeil... <a href="#"><i class="bi bi-info-circle-fill"></i> [En savoir plus]</a>
+        <i class="boxicons bx bxs-leaf"></i> Animée par une passion pour la nature et ses bienfaits, je me suis également formée en tant que conseillère en produits de soins naturels. Cette compétence complémentaire me permet de vous suggérer des solutions naturelles pour des problématiques courantes telles que l'anxiété, le stress ou les troubles du sommeil...
     </p>
 
     <h2 class="my-4" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="1500"><i class="bi bi-card-list"></i> Diplômes</h2>
@@ -35,6 +37,11 @@ export const presentationHtml = `
         <li><i class="bi bi-award"></i> 2013: Master de Psychologie clinique et Gérontologie</li>
         <li><i class="bi bi-award"></i> 2012: DU de Neuropsychologie</li>
     </ul>
+    </div>
+    <div class="right-section">
+        <img src="${img}" alt="Photo d'Aurélie"  />
+    </div>
+</div>
 
 </section>
 
